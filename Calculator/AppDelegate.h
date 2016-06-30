571f8c88b41a9ba296b45f51be0412d776b187dd
Rodgers Komen <rodgerskomen@mac-users-macbook-2.local>
2016-06-30 10:58:13 +0300

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Rodgers Komen on 30/06/2016.
+//  Copyright © 2016 strathmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
